6d658503fc4ec7da8b681afcf0951b62421258d8
GlockPL <pakerzus@tlen.pl>
2025-02-23 19:02:46 +0100

    Working history

--- rag/rag_functions.py
+++ rag/rag_functions.py
@@ -2,33 +2,34 @@ import datetime
 from typing import List, Generator, TypedDict, Annotated
 
 from langchain_core.documents import Document
-from langchain_core.messages import SystemMessage, ToolMessage, AnyMessage
+from langchain_core.messages import SystemMessage, ToolMessage, AnyMessage, HumanMessage
+from langchain_core.runnables import RunnableConfig
 from langchain_core.tools import tool
 from langchain_weaviate import WeaviateVectorStore
-from langgraph.checkpoint.memory import MemorySaver
 from langgraph.checkpoint.postgres import PostgresSaver
-from langgraph.graph import MessagesState, StateGraph, add_messages
+from langgraph.graph import StateGraph, add_messages
 from langgraph.graph import END
 from langgraph.graph.state import CompiledStateGraph
 from langgraph.prebuilt import ToolNode, tools_condition
 from psycopg import Connection
 from pydantic import BaseModel
-from db import WeaviateDB
-from utils import get_llm, get_embeddings, check_database_tables
+from db import WeaviateDB, PostgresDB
+from utils import get_llm, get_embeddings, check_database_tables, get_oai_llm
 from settings import Settings
 
 
 class RetrieveInput(BaseModel):
     query: str
 
+
 class GrapState(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
     title: str
-    last_used: datetime.datetime
 
-
-llm = get_llm()
+# llm = get_llm()
+llm = get_oai_llm()
 connection = None
+
 
 @tool(response_format="content_and_artifact", args_schema=RetrieveInput)
 def retrieve(query: str) -> tuple[str, List[Document]]:
@@ -56,9 +57,8 @@ def query_or_respond(state: GrapState) -> dict[str, list]:
     """Generate tool call for retrieval or respond."""
     llm_with_tools = llm.bind_tools([retrieve])
     response = llm_with_tools.invoke(state["messages"])
-    title = "Some Conversation"
     # MessagesState appends messages to state instead of overwriting
-    return {"messages": [response], "title": title}
+    return {"messages": [response], "title": state["title"]}
 
 
 def generate(state: GrapState) -> dict[str, list]:
@@ -89,12 +89,34 @@ def generate(state: GrapState) -> dict[str, list]:
         message
         for message in messages
         if not isinstance(message, ToolMessage) and (message.type in ("human", "system") or (
-                    message.type == 'ai' and not getattr(message, 'tool_calls', None)))
+                message.type == 'ai' and not getattr(message, 'tool_calls', None)))
     ]
-    print(f"Conversation title: {state.get('title')}")
     prompt = [SystemMessage(content=system_message_content)] + conversation_messages
     response = llm.invoke(prompt)
     return {"messages": [response], "title": state['title']}
+
+
+def name_conversation(state: GrapState, config: RunnableConfig) -> dict[str, list]:
+    """Generate a title for the conversation based on the user's first query."""
+    messages = state["messages"]
+    if not state.get("title"):
+        user_query = messages[0].content  # Assumes first message is the user query
+        prompt = [
+            SystemMessage(
+                content="Create a short, concise title for this conversation.  The title should be no more than 5 words.  Here is the user's first question:"),
+            HumanMessage(content=user_query)
+        ]
+        response = llm.invoke(prompt)
+        title = response.content
+        thread_id = config["metadata"]["thread_id"]
+
+        pg = PostgresDB()
+        pg.insert_conversation_title(thread_id, title)
+        pg.close()
+
+        return {"title": title, "messages": messages}
+
+    return {"title": state["title"], "messages": messages}
 
 def create_graph() -> CompiledStateGraph:
     global connection
@@ -102,8 +124,9 @@ def create_graph() -> CompiledStateGraph:
     graph_builder.add_node("query_or_respond", query_or_respond)
     graph_builder.add_node("tools", ToolNode([retrieve]))  # Use ToolNode
     graph_builder.add_node("generate", generate)
+    graph_builder.add_node("name_conversation", name_conversation)
 
-    graph_builder.set_entry_point("query_or_respond")
+    graph_builder.set_entry_point("name_conversation")
     graph_builder.add_conditional_edges(
         "query_or_respond",
         tools_condition,
@@ -112,6 +135,7 @@ def create_graph() -> CompiledStateGraph:
             END: END
         },
     )
+    graph_builder.add_edge("name_conversation", "query_or_respond")
     graph_builder.add_edge("tools", "generate")
     graph_builder.add_edge("generate", END)
 
@@ -119,6 +143,13 @@ def create_graph() -> CompiledStateGraph:
     settings = Settings()
 
     any_tables_in_db, tables = check_database_tables()
+    if not any_tables_in_db:
+        print("Setting up conversation titles table")
+        pg = PostgresDB()
+        pg.create_conversation_title_table()
+        pg.close()
+
+
     DB_URI = f"postgresql://{settings.user}:{settings.password}@{settings.host}:{settings.db_port}/{settings.user}?sslmode=disable"
     connection = Connection.connect(DB_URI, autocommit=True)
 
@@ -134,9 +165,9 @@ def create_graph() -> CompiledStateGraph:
     return graph
 
 
-def stream(graph: CompiledStateGraph, query: str) -> Generator[str, None, None]:
+def stream(graph: CompiledStateGraph, query: str, thread_id: str) -> Generator[str, None, None]:
     """Streams the final response tokens."""
-    config = {"configurable": {"thread_id": "1"}}
+    config = {"configurable": {"thread_id": thread_id}}
     for chunk, metadata in graph.stream({"messages": [{"role": "user", "content": query}]}, config=config,
                                         stream_mode="messages"):
         if chunk.content:
--- rag/ui.py
+++ rag/ui.py
@@ -1,44 +1,65 @@
 from datetime import datetime
-
+import random
 import streamlit as st
 
 from rag_functions import create_graph, stream
 from db import PostgresDB
 
+
+def generate_thread_id() -> str:
+    new_thread_id = datetime.now().strftime("%Y%m%d_%H%M%S")
+    return f"id_{new_thread_id}_{random.randint(0, 100000)}"
+
+
 def streamlit_app():
-    post_db = PostgresDB()
-    st.title("⚗️ ARXIV cs.AI RAG 🤖")
+    if "new_chat" in st.query_params:
+        if "current_thread_id" in st.session_state:
+            del st.session_state.current_thread_id
+
+    if "thread_id" in st.query_params:
+        st.session_state.current_thread_id = st.query_params["thread_id"]
 
     if "graph" not in st.session_state:
         st.session_state.graph = create_graph()
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
+    post_db = PostgresDB()
+    thread_history = []
+    if post_db.conversation_titles_exists():
+        thread_history = post_db.list_titles()
+    post_db.close()
+
+    if "current_thread_id" not in st.session_state:
+        new_thread_id = generate_thread_id()
+        st.session_state.current_thread_id = new_thread_id
+        st.title("⚗️ ARXIV cs.AI RAG 🤖")
+    else:
+        if st.session_state.current_thread_id in thread_history:
+            st.markdown(f"## ⚗️ {thread_history[st.session_state.current_thread_id]}")
+
     with st.sidebar:
         st.title("Chat History")
 
-        # Get all thread IDs
-        thread_history = post_db.list_history()
-        post_db.close()
+        # # Add a "New Chat" button
+        # if st.button("New Chat", key="new_chat"):
+        #     new_thread_id = generate_thread_id()
+        #     st.session_state.current_thread_id = new_thread_id
+        #     st.rerun()
 
-        # Add a "New Chat" button
-        if st.button("New Chat", key="new_chat"):
-            new_thread_id = datetime.now().strftime("%Y%m%d_%H%M%S")
-            st.session_state.current_thread_id = new_thread_id
-            st.rerun()
+        st.markdown(f'<a href="?new_chat" target="_self" style="text-decoration:none">New Chat</a>', unsafe_allow_html=True)
 
         # st.write(f"Current chat: {}")
         st.write("---")  # Divider
         st.write("Previous Conversations:")
 
         # Display each thread as a clickable link
-        for thread_id in thread_history:
-            # Create a clickable link for each conversation
-            # The query parameter will be available as st.query_params.thread_id
-            st.markdown(f'<a href="?thread_id={thread_id}" target="_self">Conversation {thread_id[:8]}...</a>',
+        for thread_id, thread_title in thread_history.items():
+            st.markdown(f'<a href="?thread_id={thread_id}" target="_self" style="text-decoration:none">{thread_title}</a>',
                         unsafe_allow_html=True)
 
-    config = {"configurable": {"thread_id": "1"}}
+    config = {"configurable": {"thread_id": st.session_state.current_thread_id}}
+
     state_history = list(st.session_state.graph.get_state_history(config))
     conversation = []
     if len(state_history) > 0:
@@ -53,7 +74,6 @@ def streamlit_app():
                 with st.chat_message("assistant"):
                     st.write(message.content)
 
-
     # for message in st.session_state.messages:
     #     with st.chat_message(message["role"]):
     #         st.markdown(message["content"])
@@ -64,7 +84,7 @@ def streamlit_app():
             st.markdown(prompt)
 
         with st.chat_message("assistant"):
-            token = stream(st.session_state.graph, prompt)
+            token = stream(st.session_state.graph, prompt, st.session_state.current_thread_id)
             response = st.write_stream(token)
         st.session_state.messages.append({"role": "assistant", "content": response})
 
